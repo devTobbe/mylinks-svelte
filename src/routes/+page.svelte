1320fea457ed9f17fc0037b509a2607a7fef5fdf
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-04-07 21:39:32 +0200

    add Button to page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import Button from '$lib/Button.svelte'
+	let buttoncontents = [
+		{text: 'Website', link: 'https://github.com/devTobbe', src: 'home.svg', srcalt: 'Little house icon'},
+		{text: 'Github', link: 'https://github.com/devTobbe', src: 'github.svg', srcalt: 'Github icon'},
+		{text: 'Website', link: 'https://github.com/devTobbe', src: 'home.svg', srcalt: 'Little house icon'},
+		{text: 'Website', link: 'https://github.com/devTobbe', src: 'home.svg', srcalt: 'Little house icon'},
+		{text: 'Website', link: 'https://github.com/devTobbe', src: 'home.svg', srcalt: 'Little house icon'}
+	]
+
+	
+</script>
+<h1>@devTobbe</h1>
+
 <p >Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+		{#each buttoncontents as buttoncontent}
+  			<Button {buttoncontent}/>
+		{/each}		
