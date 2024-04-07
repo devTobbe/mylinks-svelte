aeb063cf479f0fea4c69c2bb25e952ed5261cf45
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-04-07 21:37:55 +0200

    add custom Button

---
+++ src/lib/Button.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let buttoncontent = {
+        text: 'text',
+        link: 'http://localhost',
+        src: 'route to image',
+        srcalt: 'alt here'
+    }
+</script>
+
+<button class="bg-base2 m-11 flex align-normal px-24 py-6">
+    <a href="{buttoncontent.link}">
+        <div class="flex justify-normal">
+            <img src={buttoncontent.src} alt={buttoncontent.srcalt} class="mx-2">
+            <p>{buttoncontent.text}</p>
+        </div>
+    </a>
+</button>
