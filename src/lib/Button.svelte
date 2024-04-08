08edde5d4db7248830b77b47767245ddfcc4c408
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-04-08 21:16:23 +0200

    add proper structure and alignment

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -8,7 +8,7 @@
 </script>
 
 <button class="flex align-normal">
-    <a href="{buttoncontent.link}" class="bg-base2 m-11  px-24 py-6" target="_blank">
+    <a href="{buttoncontent.link}" class="bg-base2 m-6 px-24 py-6 rounded-xl bg-opacity-50 backdrop-blur-md" target="_blank">
         <div class="flex justify-normal">
             <img src={buttoncontent.src} alt={buttoncontent.srcalt} class="mx-2">
             <p class="font-body">{buttoncontent.text}</p>
