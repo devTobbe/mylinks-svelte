a5ddc9ae72494c53aecf6b62d940fc002390527b
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-04-08 20:47:00 +0200

    add open sans and roboto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ export default {
   content: [
     './src/**/*.{html,js,svelte,ts}'],
   theme: {
+   extend: {    
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     colors: {
       'base03': '#002b36',
       'base02': '#073642',
@@ -21,7 +38,11 @@ export default {
       'cyan': '#2aa198',
       'green': '#859900'
     },
-    extend: {},
+    fontFamily: {
+      header: ['Open+Sans', 'sans-serif'],
+      body: ['Roboto']
+    },
+ },
   },
   plugins: [],
 }
