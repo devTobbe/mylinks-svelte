0ddd7175fef250e3686c8eabec3efce8dfed364a
devTobbe <32337476+devTobbe@users.noreply.github.com>
2024-04-09 20:34:48 +0200

    add test model and light

--- src/lib/Pfp.svelte
+++ src/lib/Pfp.svelte
@@ -2,22 +2,36 @@
 
 import { onMount } from 'svelte';
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 onMount(() => {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const light = new THREE.AmbientLight( 0x404040)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+const loader = new GLTFLoader();
+
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
+scene.add(light);
 
 camera.position.z = 5;
+light.position.z = 10;
+
+loader.load('dev_tobbe.gltf', function(gltf){
+
+	scene.add(gltf.scene);
+
+}, undefined, function (error) {
+	console.error(error)
+});
 
 function animate() {
 	requestAnimationFrame( animate );
@@ -27,12 +41,9 @@ function animate() {
 
 	renderer.render( scene, camera );
 }
-
 animate();
 
-
 });  
-
 
 </script>
 
